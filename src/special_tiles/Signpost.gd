f23fb9def643fe819cd7734a5891f2d2e6a7a692
Jacob Reckhard <jacobreckhard@gmail.com>
2022-11-03 20:34:39 -0600

    added sign breaks

--- src/special_tiles/Signpost.gd
+++ src/special_tiles/Signpost.gd
@@ -10,4 +10,6 @@ func on_interact(_player):
 	emit_signal("message", message)
 
 func on_sword_hit(_player):
+	$Sign.texture = load("res://src/special_tiles/signbreak.tres")
+	yield(get_tree().create_timer(1),"timeout")
 	queue_free()
